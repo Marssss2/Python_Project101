6c4c99c01d64f315a74f03716f6d668a0bec0509
Engineer Abe <abemarasigan134@gmail.com>
2026-02-18 14:19:25 +0800

    8th Day Start 18/02/26 2:19 pm

---
+++ List.py
@@ -0,0 +1,10 @@
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+print(type(letters)) # it will print the type of the variable letters, which is <class 'list'>
+print(letters) # it will print the list of letters
+print(letters[0]) # it will print the first element of the list, which IS 'a'
+
+letter = list('hello') # it will create a list of characters from the string 'hello'
+print(letter) # it will print the list of characters, which is ['h', 'e
+
+number = list(range(1, 11)) # it will create a list of numbers from 1 to 10
+print(number) # it will print the list of numbers, which is [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
---
+++ Nested_List_Matrix.py
@@ -0,0 +1,11 @@
+matrix = [[1, 2, 3], 
+         [4, 5, 6],
+         [7, 8, 9]]
+
+
+print(matrix) # it will print the entire matrix, which is [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+mixed_matrix = [[1, 'two', 3.0],
+                ['four', 5, 6.0],
+                [7.0, 'eight', 9]]
+print(mixed_matrix) # it will print the entire mixed matrix, which is [[1, 'two', 3.0], ['four', 5, 6.0], [7.0, 'eight', 9]]
