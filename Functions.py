56042911e54e2acbaa5ba388dfed04692d82abf5
Engineer Abe <abemarasigan134@gmail.com>
2026-02-26 22:45:04 +0800

    12 day finished 26/02/26 10:44 pm

---
+++ Functions.py
@@ -0,0 +1,18 @@
+def greet():
+    print("Hello, welcome to the world of Python programming!")
+greet()
+print("This is a simple function that prints a greeting message.")
+
+def routine():
+     
+    print('Wake up early in the morning to start your day with a positive mindset.')
+    print('Exercise regularly to keep your body and mind healthy.')
+    print('Eat a balanced diet to nourish your body and boost your energy.')    
+    print('Take breaks and relax to reduce stress and improve productivity.')
+    print('Set goals and work towards achieving them to stay motivated and focused.')
+    print('Surround yourself with positive and supportive people to enhance your well-being.')
+    print('Practice gratitude and appreciate the little things in life to cultivate happiness.')
+    print('Learn new skills and expand your knowledge to grow personally and professionally.')
+    print('Take care of your mental health by seeking help when needed and practicing self-care.')
+
+routine()
